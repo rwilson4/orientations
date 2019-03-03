37143914321bcdcc32ab927e93bec95718af61f4
Bob Wilson <millstadtf@gmail.com>
2019-03-03 09:30:27 -0800

    Documenting benchmarks

---
+++ benches/README.md
@@ -0,0 +1,13 @@
+# Benchmarks
+
+Group | Function | Runtime (ns) | Throughput (Mops/s)
+------|----------|-------------:|-------------------:
+angle\_axis | angle\_axis |   45.7 |   21.9
+before\_after | after |   45.9 |   21.8
+before\_after | after\_safe |   50.8 |   19.7
+before\_after | before |   19.4 |   51.6
+cross\_product | cross\_product |    2.3 |  438.2
+dot\_product | dot\_product |    1.6 |  620.8
+from\_angle\_axis | from\_angle\_axis |   26.1 |   38.3
+inverse | inverse |    5.4 |  185.0
+inverse | inverse\_without\_check |    3.4 |  298.2
---
+++ etc/process_benchmarks.py
@@ -0,0 +1,125 @@
+from collections import OrderedDict
+from six import iteritems
+import os
+import json
+from jinja2 import Template
+
+
+def main():
+    b = {}
+    for (meta_fn, estimates_fn) in benchmark_files():
+        with open(meta_fn, 'r') as fh:
+            meta = json.loads(fh.read())
+            group = meta['group_id']
+            function = meta.get('function_id', None) or group
+
+        with open(estimates_fn) as fh:
+            contents = json.loads(fh.read())
+
+        if group not in b:
+            b[group] = {}
+            
+        b[group][function] = parse_contents(contents)
+
+    print_benchmarks(b)
+
+
+def benchmark_files():
+    """Iterator for benchmarks.
+
+    Parameters
+    ----------
+     (none)
+
+    Returns
+    -------
+     generator
+
+    Usage
+    -----
+    >>> for fn in benchmark_files():
+    ...     with open(fn, 'r') as fh:
+    ...         contents = fh.read()
+
+    """
+    this_dir, this_filename = os.path.split(__file__)
+    criterion_root = os.path.join(this_dir, '..', 'target', 'criterion')
+    for dir_name, subdir_list, file_list in os.walk(criterion_root):
+        if os.path.split(dir_name)[-1] != 'new':
+            continue
+
+        meta = os.path.join(dir_name, 'benchmark.json')
+        estimates = os.path.join(dir_name, 'estimates.json')
+        yield (meta, estimates)
+
+
+def parse_contents(contents):
+    """Extract information from benchmark.
+
+    Parameters
+    ----------
+     contents : dict_like
+        Benchmark data.
+
+    Returns
+    -------
+     data : dictionary
+        Relevant benchmark data.
+
+    """
+    data = {
+        'Runtime': contents['Slope']['point_estimate'],
+        'Throughput': 1000. / contents['Slope']['point_estimate']
+    }
+    return data
+
+
+def print_benchmarks(benchmarks):
+    """Use jinja to generate benchmark summary.
+
+    Parameters
+    ----------
+     benchmarks : dict_like
+        Keys are names for benchmarks, values are the
+        results. Name/subname?
+
+    Returns
+    -------
+     (nothing)
+
+    """
+
+    tmpl = """# Benchmarks
+
+Group | Function | Runtime (ns) | Throughput (Mops/s)
+------|----------|-------------:|-------------------:
+{%- for grp_name, grp in benchmarks.items() %}
+{%- for fn_name, res in grp.items() %}
+{{
+   grp_name | replace("_", "\_")
+}} | {{
+   fn_name | replace("_", "\_")
+}} | {{
+   '%6.1f'| format(res['Runtime']|float)
+}} | {{
+   '%6.1f'| format(res['Throughput']|float)
+}}
+{%- endfor %}
+{%- endfor %}"""
+
+    template = Template(tmpl)
+
+    # Sort benchmarks
+    for k, v in iteritems(benchmarks):
+        benchmarks[k] = OrderedDict(sorted(iteritems(v)))
+
+    benchmarks = OrderedDict(sorted(iteritems(benchmarks)))
+
+    this_dir, this_filename = os.path.split(__file__)
+    bfn = os.path.join(this_dir, '..', 'benches', 'README.md')
+    with open(bfn, 'w') as fh:
+        fh.write(template.render(benchmarks=benchmarks))
+
+
+if __name__ == '__main__':
+    main()
